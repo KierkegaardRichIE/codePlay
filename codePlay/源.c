0580cf5834e7845481e3262789feb169931a7a56
KierkegaardRichIE <kierke7@outlook.com>
2019-08-03 08:27:29 +0800

    测试自动变量和静态变量

--- codePlay/源.c
+++ codePlay/源.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
-
+/*
 int main(){
 	int c, lastc;
 	int year = 0;
-/*
+
 	lastc = 'a';
 	while((c=getchar())!=EOF){
 		if(c!=' ')
@@ -14,11 +14,32 @@ int main(){
 		lastc = c;
 	}
 */
+//2-4
+/*
 	scanf("%d", &year);
 	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 		printf("%d is a leap year!\n", year);
 	else
 		printf("%d is not a leap year!\n", year);
 	getchar();
+	return 0;
+}
+*/
+//自动变量和静态变量
+void test()
+{
+	auto int a = 0;
+	static int b = 7;
+	a++;
+	b++;
+	printf("%d ", a);
+	printf("%d\t", b);
+}
+int main()
+{
+	for (int i = 0; i < 7; i++)
+	{
+		test();
+	}
 	return 0;
 }
