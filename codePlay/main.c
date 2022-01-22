1686fe088e79063f896871fd7f21b3ba193825c6
祁克果 <kierke7@outlook.com>
2022-01-22 23:41:42 +0800

    🧨Functionalize the program

--- codePlay/main.c
+++ codePlay/main.c
@@ -1,15 +1,23 @@
 ﻿#include <stdio.h>
 
+void sum(int begin, int end) {
+	int i;
+	int sum = 0;
+}
+int bitsEstimate(int source) {
+	int n = 0;
+	while (source > 0) {
+		n++;
+		source /= 10;
+	}
+	return n;
+}
 int main(int argc, char* argv[]) {
+	int m = 0;
 	unsigned long x;
-	long n = 0;
 	printf("Input the number you want to count: ");
 	scanf_s("%u", &x);
-	while (x>0)
-	{
-		n++;
-		x /= 10;
-	}
-	printf("The number has %d digits\n", n);
+	m = bitsEstimate(x);
+	printf("The number has %d digits\n", m);
 	return 0;
 }
